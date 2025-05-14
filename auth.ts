6ed3fec800244d4f8ec5603ac9851b8b591cfb8a
Joey <1508224503@qq.com>
2025-05-14 19:38:39 +0800

    add secret in auth

--- auth.ts
+++ auth.ts
@@ -20,6 +20,8 @@ async function getUser(email: string): Promise<User | undefined> {
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  secret: process.env.AUTH_SECRET,
+  trustHost: true,
   providers: [
     Credentials({
       async authorize(credentials) {
