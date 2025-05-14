c5473e3a68f9572281a5697c8a7315bfa5cd75b7
Joey <1508224503@qq.com>
2025-05-14 15:41:50 +0800

    dix build issue: customers page props should work as Promise.

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -6,16 +6,14 @@ export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: {
+export default async function Page(props: {
+  searchParams?: Promise<{
     query?: string;
     page?: string;
-  };
+  }>;
 }) {
+  const searchParams = await props.searchParams;
   const query = searchParams?.query || '';
-
   const customers = await fetchFilteredCustomers(query);
 
   return (
